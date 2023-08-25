dcf4ae4f81ddc9f50a36a82f9ad80fbcf8c65b86
Waqar Mastan <waqarmaxtan@gmail.com>
2023-08-25 11:51:22 +0500

    Handle GET/POST methods in same handler

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,25 @@
-const login = (req, res) => {
+const renderLoginTemplate = (req, res) => {
   res.render("login");
 };
 
-const signup = (req, res) => {
+const login = (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.redirect("/");
+};
+
+const renderSignupTemplate = (req, res) => {
   res.render("signup");
+};
+const signup = (req, res) => {
+  const { fullname, email, password, confirmPassword } = req.body;
+  console.log(fullname, email, password, confirmPassword);
+  res.redirect("/");
 };
 
 module.exports = {
   login,
   signup,
+  renderLoginTemplate,
+  renderSignupTemplate,
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,14 @@ const router = require("express").Router();
 // local imports
 const authController = require("../controllers/auth.controller");
 
-router.get("/signup", authController.signup);
-router.get("/login", authController.login);
+router
+  .route("/signup")
+  .get(authController.renderSignupTemplate)
+  .post(authController.signup);
+
+router
+  .route("/login")
+  .get(authController.renderLoginTemplate)
+  .post(authController.login);
 
 module.exports = router;
