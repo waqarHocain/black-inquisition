b35e2ab0da24e3d64791434def98bd8d8b499288
Waqar Mastan <waqarmaxtan@gmail.com>
2023-08-31 22:30:42 +0500

    Add server side validtion user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const validateEmail = require("../utils/validateEmail");
+
 const renderLoginTemplate = (req, res) => {
   res.render("login");
 };
@@ -12,9 +14,27 @@ const renderSignupTemplate = (req, res) => {
   res.render("signup");
 };
 const signup = (req, res) => {
-  const { fullname, email, password, confirmPassword } = req.body;
-  console.log(fullname, email, password, confirmPassword);
-  res.redirect("/");
+  // TODO: prepopulate form in case of errors
+  const { fullname, email, password, confirmPassword, bio } = req.body;
+
+  // data validation
+  const errors = {};
+  if (fullname.length < 4)
+    errors.fullname = "Username must be 4 or more characters long";
+  if (password.length < 8)
+    errors.password = "Password must be 8 or more characters long";
+  if (password !== confirmPassword)
+    errors.confirmPassword = "Passwords don't match";
+  if (!validateEmail(email)) errors.email = "Email is not valid";
+  if (bio.length < 20) errors.bio = "Bio must be 20 or more characters long";
+
+  if (Object.keys(errors).length !== 0) {
+    res.locals.errors = errors;
+    res.locals.formData = { fullname, email, password, confirmPassword, bio };
+    res.render("signup");
+  } else {
+    res.redirect("/");
+  }
 };
 
 module.exports = {
