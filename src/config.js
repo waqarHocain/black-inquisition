4e1b2c671c74f2db77a25579fcc53860b450abd5
Waqar Mastan <waqarmaxtan@gmail.com>
2023-09-02 21:32:41 +0500

    Add cookie-session middleware

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 module.exports = {
   PORT,
+  SESSION_SECRET,
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cookieSession = require("cookie-session");
 
 // local imports
 const publicRouter = require("./routes/public");
@@ -11,6 +12,18 @@ const authRouter = require("./routes/auth");
 const config = require("./config");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    secret: config.SESSION_SECRET,
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    httpOnly: true,
+  })
+);
+
+// enable cookies when serving behind a proxy
+app.enable("trust proxy");
 
 // set different http headers for security
 app.use(helmet());
