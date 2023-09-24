dcdc1599d307c76f3c031b26fdd0ae603a60b7ed
Waqar Mastan <waqarmaxtan@gmail.com>
2023-09-24 11:27:02 +0500

    Add view for choosing role

--- src/controllers/public.controller.js
+++ src/controllers/public.controller.js
@@ -6,7 +6,12 @@ const updates = (req, res) => {
   res.render("updates");
 };
 
+const chooseRole = (req, res) => {
+  res.render("signupAs");
+};
+
 module.exports = {
   homepage,
   updates,
+  chooseRole,
 };
--- src/routes/public.js
+++ src/routes/public.js
@@ -5,5 +5,6 @@ const publicController = require("../controllers/public.controller");
 
 router.get("/", publicController.homepage);
 router.get("/updates", publicController.updates);
+router.get("/signup-as", publicController.chooseRole);
 
 module.exports = router;
