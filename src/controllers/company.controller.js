e7fefd152dce3c08f0ceb00691ccdea9c8f6a06b
Waqar Mastan <waqarmaxtan@gmail.com>
2023-09-29 22:38:46 +0500

    Create job

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -22,7 +22,62 @@ const profile = async (req, res) => {
   res.render("companyProfile", { company });
 };
 
+const renderCreateJobTemplate = (req, res) => {
+  return res.render("createJob");
+};
+
+const createJob = async (req, res) => {
+  const { title, company, location, workplace, jobType, description } =
+    req.body;
+
+  const errors = {};
+
+  if (title.trim().length < 8)
+    errors.title = "Title must be 8 or more characters long";
+  if (!company.trim()) errors.company = "Please enter company name";
+  if (!location.trim()) errors.location = "Please enter a valid location";
+  if (description.trim().length < 30)
+    errors.description = "Job description must be 30 or more charcters long";
+
+  const workplaceOptions = ["onSite", "hybrid", "remote"];
+  const jobTypeOptions = [
+    "fullTime",
+    "partTime",
+    "contract",
+    "temporary",
+    "volunteer",
+    "internship",
+    "other",
+  ];
+
+  if (!workplaceOptions.includes(workplace))
+    errors.workplace = "Invalid workplace type";
+  if (!jobTypeOptions.includes(jobType)) errors.jobType = "Invalid job type";
+
+  // if there're errors, render template with errors
+  if (Object.keys(errors).length !== 0) {
+    res.locals.errors = errors;
+    return res.render("createJob");
+  }
+
+  const job = await db.job.create({
+    data: {
+      title,
+      location,
+      company,
+      workplace,
+      description,
+      type: jobType,
+      companyId: req.session.id,
+    },
+  });
+
+  return res.redirect("/company/profile");
+};
+
 module.exports = {
   getJobs,
+  renderCreateJobTemplate,
+  createJob,
   profile,
 };
--- src/routes/company.js
+++ src/routes/company.js
@@ -4,6 +4,10 @@ const router = require("express").Router();
 const companyController = require("../controllers/company.controller");
 
 router.route("/jobs").get(companyController.getJobs);
+router
+  .route("/jobs/new")
+  .get(companyController.renderCreateJobTemplate)
+  .post(companyController.createJob);
 router.route("/profile").get(companyController.profile);
 
 module.exports = router;
