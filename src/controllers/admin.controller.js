56d151a64657e159428d9c7f9077e6d4dd98bbc4
Waqar Mastan <waqarmaxtan@gmail.com>
2023-10-03 13:27:19 +0500

    Lets admin approve / decline company accounts

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -9,6 +9,50 @@ const renderDashboard = async (req, res) => {
   return res.render("adminDashboard", { companies });
 };
 
+const renderCompanyProfile = async (req, res) => {
+  const { companyId } = req.params;
+
+  const company = await db.company.findUnique({
+    where: {
+      id: companyId,
+    },
+    include: {
+      sources: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  if (!company) return res.sendStatus(404);
+
+  return res.render("verifyAccount", { company });
+};
+
+const approveCompany = async (req, res) => {
+  const { companyId } = req.params;
+  await db.company.update({
+    where: { id: companyId },
+    data: {
+      verified: true,
+    },
+  });
+  return res.redirect("/admin/dashboard");
+};
+
+const deleteCompany = async (req, res) => {
+  const { companyId } = req.params;
+  await db.company.delete({
+    where: {
+      id: companyId,
+    },
+  });
+  return res.redirect("/admin/dashboard");
+};
+
 module.exports = {
   renderDashboard,
+  renderCompanyProfile,
+  approveCompany,
+  deleteCompany,
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,5 +4,9 @@ const router = require("express").Router();
 const adminController = require("../controllers/admin.controller");
 
 router.get("/dashboard", adminController.renderDashboard);
+router.get("/dashboard/:companyId", adminController.renderCompanyProfile);
+
+router.post("/dashboard/:companyId/approve", adminController.approveCompany);
+router.post("/dashboard/:companyId/decline", adminController.deleteCompany);
 
 module.exports = router;
