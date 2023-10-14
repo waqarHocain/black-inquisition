08844b72bb335747e44cda3f67a3f82ff3ad0674
Waqar Mastan <waqarmaxtan@gmail.com>
2023-10-15 00:34:02 +0500

    Check verified status of account bug fix

--- src/middleware/checkVerified.js
+++ src/middleware/checkVerified.js
@@ -1,6 +1,50 @@
-const checkVerified = (req, res, next) => {
+const db = require("../services/db");
+const jwt = require("../services/jwt");
+const { ROLES } = require("../config");
+
+const checkVerified = async (req, res, next) => {
   if (req.user && req.user.verified) {
     return next();
+  }
+
+  // refetch latest user state
+  if (req.user.role === ROLES.USER) {
+    const user = await db.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
+
+    if (user && user.verified) {
+      // update value in session
+      const token = jwt.generateToken({
+        id: req.user.id,
+        email: req.user.email,
+        role: req.user.role,
+        verified: true,
+      });
+      req.session.token = token;
+      return next();
+    }
+  } else if (req.user.role === ROLES.COMPANY) {
+    const company = await db.company.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
+
+    if (company && company.verified) {
+      // update value in session
+      const token = jwt.generateToken({
+        id: req.user.id,
+        email: req.user.email,
+        role: req.user.role,
+        verified: true,
+      });
+      req.session.token = token;
+
+      return next();
+    }
   }
   return res.redirect("/account/unverified");
 };
--- src/routes/account.js
+++ src/routes/account.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
+const db = require("../services/db");
+const { ROLES } = require("../config");
 
-router.get("/unverified", (req, res) => {
-  return res.render("unverified");
+router.get("/unverified", async (req, res) => {
+  if (!req.user.verified) {
+    // fetch fresh state from db
+    if (req.user.role === ROLES.USER) {
+      const user = await db.user.findUnique({
+        where: {
+          id: req.user.id,
+        },
+      });
+
+      if (user && user.verified) return res.redirect("/user/profile");
+    } else if (req.user.role === ROLES.COMPANY) {
+      const company = await db.company.findUnique({
+        where: {
+          id: req.user.id,
+        },
+      });
+
+      if (company && company.verified) return res.redirect("/company/profile");
+    }
+    return res.render("unverified");
+  }
+
+  return res.redirect("/");
 });
 
 module.exports = router;
