8aabf864558bc3a1b8c2c504f470b24886af9c60
Waqar <waqarmaxtan@gmail.com>
2023-12-08 23:38:16 +0500

    Add Post model

---
+++ prisma/migrations/20231208183647_post_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "title" STRING NOT NULL,
+    "body" STRING NOT NULL,
+    "companyId" INT8,
+    "userId" INT8,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
