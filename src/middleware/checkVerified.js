5b057327d04bc7421ca0524852e3835e5f3b3c31
Waqar <waqarmaxtan@gmail.com>
2023-12-16 23:28:49 +0500

    Update middleware for new schema models

--- src/middleware/checkVerified.js
+++ src/middleware/checkVerified.js
@@ -8,44 +8,24 @@ const checkVerified = async (req, res, next) => {
   }
 
   // refetch latest user state
-  if (req.user.role === ROLES.USER) {
-    const user = await db.user.findUnique({
-      where: {
-        id: req.user.id,
-      },
+  const user = await db.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+  });
+
+  if (user && user.verified) {
+    // update value in session
+    const token = jwt.generateToken({
+      id: req.user.id,
+      email: req.user.email,
+      role: req.user.role,
+      verified: true,
     });
-
-    if (user && user.verified) {
-      // update value in session
-      const token = jwt.generateToken({
-        id: req.user.id,
-        email: req.user.email,
-        role: req.user.role,
-        verified: true,
-      });
-      req.session.token = token;
-      return next();
-    }
-  } else if (req.user.role === ROLES.COMPANY) {
-    const company = await db.company.findUnique({
-      where: {
-        id: req.user.id,
-      },
-    });
-
-    if (company && company.verified) {
-      // update value in session
-      const token = jwt.generateToken({
-        id: req.user.id,
-        email: req.user.email,
-        role: req.user.role,
-        verified: true,
-      });
-      req.session.token = token;
-
-      return next();
-    }
+    req.session.token = token;
+    return next();
   }
+
   return res.redirect("/account/unverified");
 };
 
