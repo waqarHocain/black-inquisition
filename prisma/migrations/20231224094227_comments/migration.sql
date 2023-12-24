4379f235225887c88bfe18fbd44b7be59c837a64
Waqar <waqarmaxtan@gmail.com>
2023-12-24 14:43:31 +0500

    Add Comment model

---
+++ prisma/migrations/20231224094227_comments/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "body" STRING NOT NULL,
+    "postId" INT8 NOT NULL,
+    "userId" INT8 NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
