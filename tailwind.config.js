ff166c0032c61b6df1f7a4bbfbac63502548579c
Waqar <waqarmaxtan@gmail.com>
2024-01-01 22:08:37 +0500

    Change ui to a dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", "sans-serif"],
       },
+      colors: {
+        lightBlack: "#111827",
+        lightBlue: "#617BFF",
+        blueDark: "#4766FF",
+        cream: "#FEF9FF",
+        golden: "#F5B841",
+      },
     },
   },
   plugins: [],
