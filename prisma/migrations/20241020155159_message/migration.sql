55883aa233bc8e479f67df52cf9c2c9223a21ce1
Waqar Mastan <waqarmaxtan@gmail.com>
2024-10-20 21:30:34 +0500

    Message model

---
+++ prisma/migrations/20241020155159_message/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "fromUserId" INT8 NOT NULL,
+    "toUserId" INT8 NOT NULL,
+    "message" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_fromUserId_fkey" FOREIGN KEY ("fromUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_toUserId_fkey" FOREIGN KEY ("toUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
