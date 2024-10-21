d267e9071a949df5f6d3752581af87c9c56b5b54
Waqar Mastan <waqarmaxtan@gmail.com>
2024-10-21 22:39:21 +0500

    Template for listing messages

--- src/controllers/messages.controller.js
+++ src/controllers/messages.controller.js
@@ -34,7 +34,42 @@ const msgUser = async (req, res) => {
 };
 
 const listChats = async (req, res) => {
-  res.json({ msg: "ok" });
+  const messages = await db.message.findMany({
+    where: {
+      OR: [
+        {
+          fromUserId: req.session.id,
+        },
+        {
+          toUserId: req.session.id,
+        },
+      ],
+    },
+  });
+  if (messages.length === 0) return res.render("messages", { users: null });
+
+  const ids = [];
+  const currentUserId = BigInt(req.session.id);
+  messages.forEach((msg) => {
+    if (!ids.includes(msg.fromUserId) && msg.fromUserId !== currentUserId)
+      ids.push(msg.fromUserId);
+    if (!ids.includes(msg.toUserId) && msg.toUserId !== currentUserId)
+      ids.push(msg.toUserId);
+  });
+  const users = await db.user.findMany({
+    where: {
+      id: {
+        in: ids,
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      photo: true,
+    },
+  });
+
+  res.render("messages", { users });
 };
 
 module.exports = {
